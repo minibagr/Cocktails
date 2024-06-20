1bad67ecbd8dc0331b8bfc6ad61f8d4260a0ab01
Samuel Huf <samhuf@seznam.cz>
2024-06-20 10:35:39 +0200

    Udelane vyhledavani

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='
+const INGREDIENTS_URL = 'www.thecocktaildb.com/images/ingredients/'
 const data_div = document.getElementById('data')
+const search = document.getElementById('search')
 
+let data_list = []
 let alphabet = [
     'a',
     'b',
@@ -30,42 +33,68 @@ let alphabet = [
     'z'
 ]
 
-async function get_data() {
-    alphabet.forEach((letter) => {
-        bla(letter)
-    })
+search.addEventListener('input', (e) => {
+    e.preventDefault()
+    for (let k of data_list) {
+        if (
+            !k.childNodes[1].textContent
+                .toLowerCase()
+                .includes(search.value.toLowerCase())
+        ) {
+            k.setAttribute('class', 'coctail none')
+        } else {
+            k.setAttribute('class', 'coctail show')
+        }
+    }
+})
+
+function filter_idk(a) {
+    console.log(a)
 }
 
-async function bla(letter) {
-    await fetch(BASE_URL + letter)
-        .then((response) => response.json())
-        .then((data) => get_objects(data.drinks))
+async function get_data() {
+    for await (const letter of alphabet) {
+        await fetch(BASE_URL + letter)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.drinks !== null) get_objects(data.drinks)
+            })
+    }
+
+    create_list()
 }
+
+async function bla(letter) {}
 
 async function get_objects(data) {
-    data.forEach((drink) => {
+    await data.forEach(async (drink) => {
         create_blocks(drink)
     })
 }
 
-async function create_blocks(data) {
+function create_blocks(data) {
     const div = document.createElement('div')
-    const p = document.createElement('p')
     const h1 = document.createElement('h1')
     const img = document.createElement('img')
 
     img.src = data.strDrinkThumb
     h1.textContent = data.strDrink
-    p.textContent = data.strInstructions
-
-    console.log(data)
 
     div.appendChild(img)
     div.appendChild(h1)
-    div.appendChild(p)
 
-    div.setAttribute('id', data.objectID)
+    div.setAttribute('id', data.idDrink)
+    div.setAttribute('class', 'coctail show')
     data_div.appendChild(div)
 }
+
+async function create_list() {
+    data_list = document.getElementsByClassName('coctail')
+    console.log('nacteno')
+}
+
+window.addEventListener('load', () => {
+    search.value = ''
+})
 
 get_data()
