dfcd65683f4946bfc9d5a3d52288334b35820879
Samuel Huf <samhuf@seznam.cz>
2024-06-20 13:37:55 +0200

    Pridani ingredienci, meni se barva ale nic to nedela, potreba udelata funkcnost na filtraci podle ingredienci

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='
 const INGREDIENTS_URL = 'www.thecocktaildb.com/images/ingredients/'
 const data_div = document.getElementById('data')
 const search = document.getElementById('search')
+const loading = document.getElementById('loading')
+const ingredients_div = document.getElementById('ingredients-div')
 
+let ingredients_list = []
 let data_list = []
 let alphabet = [
     'a',
@@ -41,16 +44,12 @@ search.addEventListener('input', (e) => {
                 .toLowerCase()
                 .includes(search.value.toLowerCase())
         ) {
-            k.setAttribute('class', 'coctail none')
+            k.setAttribute('class', 'cocktail none')
         } else {
-            k.setAttribute('class', 'coctail show')
+            k.setAttribute('class', 'cocktail')
         }
     }
 })
-
-function filter_idk(a) {
-    console.log(a)
-}
 
 async function get_data() {
     for await (const letter of alphabet) {
@@ -61,10 +60,9 @@ async function get_data() {
             })
     }
 
+    await create_ingredients()
     create_list()
 }
-
-async function bla(letter) {}
 
 async function get_objects(data) {
     await data.forEach(async (drink) => {
@@ -72,29 +70,67 @@ async function get_objects(data) {
     })
 }
 
-function create_blocks(data) {
+async function create_blocks(data) {
     const div = document.createElement('div')
     const h1 = document.createElement('h1')
     const img = document.createElement('img')
 
-    img.src = data.strDrinkThumb
+    let ingredients = get_ingredients(data)
+
+    img.src = data.strDrinkThumb + '/preview'
     h1.textContent = data.strDrink
+
+    div.dataset.ingredients = ingredients
 
     div.appendChild(img)
     div.appendChild(h1)
 
     div.setAttribute('id', data.idDrink)
-    div.setAttribute('class', 'coctail show')
+    div.setAttribute('class', 'cocktail show')
     data_div.appendChild(div)
 }
 
-async function create_list() {
-    data_list = document.getElementsByClassName('coctail')
-    console.log('nacteno')
+function create_ingredients() {
+    ingredients_list = ingredients_list.sort()
+    ingredients_list = ingredients_list.splice(1, ingredients_list.length)
+    for (const ingredient of ingredients_list) {
+        const p = document.createElement('p')
+
+        p.textContent = ingredient
+
+        p.addEventListener('click', (e) => {
+            p.classList.toggle('selected')
+        })
+
+        p.setAttribute('class', 'ingredient')
+
+        ingredients_div.appendChild(p)
+    }
 }
 
-window.addEventListener('load', () => {
+function get_ingredients(data) {
+    let ingredients = ''
+    for (let i = 1; i <= 15; i++) {
+        let ingredient = data['strIngredient' + i]
+        if (ingredient !== null) {
+            ingredients += ingredient + ','
+
+            if (!ingredients_list.includes(ingredient)) {
+                ingredients_list.push(ingredient)
+            }
+            continue
+        }
+        break
+    }
+    return ingredients.slice(0, ingredients.length - 1)
+}
+
+async function create_list() {
+    data_list = document.getElementsByClassName('cocktail')
     search.value = ''
-})
+    document.body.removeAttribute('id')
+    loading.removeAttribute('id')
+    loading.setAttribute('class', 'none')
+}
 
 get_data()
